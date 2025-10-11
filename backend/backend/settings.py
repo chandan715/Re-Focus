531fc691549a3239783381c6d5c7205e66234bb9
Chandan Nayak <nayakchandan0271@gmail.com>
2025-10-11 16:39:57 +0530

    ready for deploy using versal

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # DRF
     'rest_framework_simplejwt.token_blacklist', # For JWT blacklisting
-    'django_cors_headers',  # CORS support
+    'corsheaders',  # CORS support
     'api',  # our app
 ]
 
